078d1a23e248b440ee3fc99802cb90faa35bfa7a
jaywcjlove <398188662@qq.com>
2022-01-08 17:57:17 +0800

    chore(deps): update dependency kkt to v7.
    Upgrade react-scripts to v5, Support Webpack 5.x
    https://github.com/kktjs/kkt/issues/198

--- .kktrc.ts
+++ .kktrc.ts
@@ -13,31 +13,45 @@ export default (conf: Configuration, env: 'production' | 'development', options:
     }),
   );
 
-  conf.optimization = {
-    ...conf.optimization,
-    splitChunks: {
-      cacheGroups: {
-        reactvendor: {
-          test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
-          name: 'react-vendor',
-          chunks: 'all',
-        },
-        katex: {
-          test: /[\\/]node_modules[\\/](katex)[\\/]/,
-          name: 'katex-vendor',
-          chunks: 'all',
-        },
-        prismjs: {
-          test: /[\\/]node_modules[\\/](prismjs)[\\/]/,
-          name: 'prismjs-vendor',
-          chunks: 'async',
-        },
-      },
-    },
-  };
-
   if (env === 'production') {
     conf.output = { ...conf.output, publicPath: './' };
+    conf.optimization = {
+      ...conf.optimization,
+      splitChunks: {
+        cacheGroups: {
+          reactvendor: {
+            test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
+            name: 'react-vendor',
+            chunks: 'all',
+          },
+          katex: {
+            test: /[\\/]node_modules[\\/](katex)[\\/]/,
+            name: 'katex-vendor',
+            chunks: 'all',
+          },
+          prismjs: {
+            test: /[\\/]node_modules[\\/](refractor)[\\/]/,
+            name: 'refractor-vendor',
+            chunks: 'all',
+          },
+          codemirror: {
+            test: /[\\/]node_modules[\\/](@codemirror)[\\/]/,
+            name: 'codemirror-vendor',
+            chunks: 'all',
+          },
+          uiw: {
+            test: /[\\/]node_modules[\\/](@uiw)[\\/]/,
+            name: 'uiw-vendor',
+            chunks: 'all',
+          },
+          parse5: {
+            test: /[\\/]node_modules[\\/](parse5)[\\/]/,
+            name: 'parse5-vendor',
+            chunks: 'all',
+          },
+        },
+      },
+    };
   }
   return conf;
 };
